a2218adcf2d180efaea782c3fd33aa68331c4f0a
Antonysibi <87759135+Antonysibi@users.noreply.github.com>
2021-08-04 09:21:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,22 @@
-var ship,ship_sailing,edges;
+var ship;
+var ship_sailing;
 var seaImg;
 var sea;
 function preload(){
 seaImg = loadImage("sea.png");
-ship_sailing= loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+ship_sailing = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
 }
 
 function setup(){
   createCanvas(400,400);
-  shipImg = createSprite(200,200,150,150)
-  shipImg.addAnimation("sailing",seaImg);
+  ship = createSprite(200,200,150,150)
+  ship.addAnimation("sailing",seaImg);
   
   ship.scale = 0.5;
   ship.x = 50
 
-  sea = createSprite(200,180,400,20);
-  sea.addImage("sea",seaImg);
+  seaImg = createSprite(200,180,400,20);
+  seaImg.addImage("sea",seaImg);
   ground.x = ground.width/2;
 }
 
